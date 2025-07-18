e41faf812e515f33f89e41d6797a630d336bbc87
its-krishna-patil <115216743+its-krishna-patil@users.noreply.github.com>
2025-07-18 20:26:45 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,8 @@
+// src/api.js
+import axios from "axios";
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
-  withCredentials: true,
 });
+
+export default API;
